4f9e1c14dddd9fc212670e3ef4ed76dfc0b93646
DerrickQin <si_yuan_qin@163.com>
2016-10-31 17:20:40 +0800

    项目初始化

---
+++ CherrysGift/CherrysGift/Classes/Others/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CherrysGift
+//
+//  Created by admin on 2016/10/31.
+//  Copyright © 2016年 Derrick_Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
